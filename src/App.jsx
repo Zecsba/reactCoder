3ae83497f18f77469f180f6cc4fda9ef74215f9f
Sebastian Casallas <zecsba2019casarias@gmail.com>
2022-05-16 17:30:01 -0500

    Se agrego el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
+import NavBar from './components/NavBar';
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <>
       <NavBar />
-      <Footer />
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return (
-        <h2>Desde el footer</h2>
-    )
-}
-
-export default Footer
