150257846100c93653b9cd9281eca5675adaa1d7
Sebastian Casallas <zecsba2019casarias@gmail.com>
2022-05-26 20:37:36 -0500

    Item y ItemList se le hicieron cambios.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,27 +1,18 @@
 import Spiner from "../icons/Spiner"
+import Item from './Item'
 
 const ItemList = ({items}) =>{
     return(
-        <div className="flex justify-center flex-wrap h-screen overflow-y-scroll">
+        <div className="flex justify-center flex-wrap h-[50rem] overflow-y-scroll">
             { items.length > 0
                 ? items.map(item => 
-                <div key={item.id} className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-stone-700 dark:border-gray-700 m-8">
-                    <div className="flex justify-center mt-3">
-                        <img className="rounded-t-lg" src={item.src} alt="" />
-                    </div>
-
-                    <div className="p-5">
-                        <div>
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-                        </div>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.descriptionOne}</p>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.descriptionTwo}</p>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.descriptionThree}</p>
-                    </div>
-
-                    <div className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{item.price}</div>
-                    </div>
-                ) : <div className="text-center"><Spiner /></div>
+                    <Item
+                        key={item.id} 
+                        items={item}
+                    />                  
+                ) 
+                :
+                <div className="text-center"><Spiner /></div>
             }
         </div>
     )
