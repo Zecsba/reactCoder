7dc1f4aac0c883ec963e0ceb65f4ff8e3481379b
Sebastian Casallas <zecsba2019casarias@gmail.com>
2022-06-16 23:56:00 -0500

    Las categorias sirven ya

--- src/utils/FirestoreFetch.jsx
+++ src/utils/FirestoreFetch.jsx
@@ -2,14 +2,15 @@ import { query, orderBy, where, collection, getDocs } from '@firebase/firestore'
 import { doc, getDoc } from "firebase/firestore";
 import db from './firebaseConfig';
 
-export const firestoreFetch = async (idcategory) => {
+export const firestoreFetch = async (idCategory) => {
     let q;
-    if (idcategory) {
-        const categoryDocRef = doc(db, "categories", idcategory);
-        q = query( collection(db, "products"), where("idcategory", "==", categoryDocRef) );
-    } else {
-        q = query(collection(db, "products"));
-    }
+    if (idCategory) {
+      q = query(
+        collection(db, "products"), where("idCategory", "==", idCategory)
+       );
+  } else {
+      q = query(collection(db, "products"));
+  }
     const querySnapshot = await getDocs(q);
     const dataFromFirestore = querySnapshot.docs.map(document => ({
         id: document.id,
